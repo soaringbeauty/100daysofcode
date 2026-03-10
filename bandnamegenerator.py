b01091e13de3cb00ca9503ef51fab329c2493afa
soaringbeauty <106130020+soaringbeauty@users.noreply.github.com>
2026-03-10 11:41:04 -0300

    Add band name generator script
    Task 1

---
+++ bandnamegenerator.py
@@ -0,0 +1,12 @@
+# Intro
+print("Welcome to the Band Name Generator!")
+
+# Ask for the user's name and greet them in a single line
+print("Hello there, " + input("What is your name? ") + "!")
+
+# Ask for city and pet
+city = input("What city did you grew up in?\n")
+pet = input("Could you name a pet?\n")
+
+# Output
+print("Your band name could be " + city + " " + pet + "!")
